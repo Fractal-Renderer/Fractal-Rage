4f92e74d0080ab3245456b13191f5232b4441c77
Jadefalke2 <79963380+Jadefalke2@users.noreply.github.com>
2022-01-13 16:56:36 +0100

    Simple render based of yet non-existant fractal

---
+++ src/main/java/model/Bounds.java
@@ -0,0 +1,5 @@
+package model;
+
+import lombok.RequiredArgsConstructor;
+
+public record Bounds(double x, double y, double width, double height) {}
--- src/main/java/model/FractalRenderer.java
+++ src/main/java/model/FractalRenderer.java
@@ -1,4 +1,37 @@
 package model;
 
+import lombok.Setter;
+
+import java.awt.image.BufferedImage;
+
 public class FractalRenderer {
+
+    @Setter
+    private Fractal fractal;
+
+    public FractalRenderer(Fractal fractal) {
+        this.fractal = fractal;
+    }
+
+    public FractalRenderer() {
+        this(null);
+    }
+
+    public BufferedImage render(Bounds bounds, Resolution res) {
+        if (fractal == null) {
+            throw new IllegalStateException("Cannot render null fractal");
+        }
+
+        BufferedImage image = new BufferedImage(res.getWidth(), res.getHeight(), BufferedImage.TYPE_INT_RGB);
+        double stepSizeH = res.getHeight() / bounds.height();
+        double stepSizeW = res.getWidth() / bounds.width();
+
+        for (int i = 0; i < image.getWidth(); i++) {
+            for (int j = 0; j < image.getHeight(); j++) {
+                image.setRGB(i, j, fractal.getColorAt(i * stepSizeW, j * stepSizeH).getRGB());
+            }
+        }
+
+        return image;
+    }
 }
