8873f29d54db40cd5c7183c92cbbde19ed219dd9
Jadefalke <79963380+Jadefalke2@users.noreply.github.com>
2022-03-20 17:18:36 +0100

    transfer listener

--- src/main/java/model/FractalRenderer.java
+++ src/main/java/model/FractalRenderer.java
@@ -21,18 +21,18 @@ public class FractalRenderer {
         this(null);
     }
 
-    public BufferedImage render(AtomicReference<Bounds> bounds, Resolution res) {
+    public BufferedImage render(Bounds bounds, Resolution res) {
         if (fractal == null) {
             throw new IllegalStateException("Cannot render null fractal");
         }
 
         BufferedImage image = new BufferedImage(res.width(), res.height(), BufferedImage.TYPE_INT_RGB);
-        double stepSizeH = bounds.get().height() / res.height();
-        double stepSizeW = bounds.get().width() / res.width();
+        double stepSizeH = bounds.height() / res.height();
+        double stepSizeW = bounds.width() / res.width();
 
         for (int i = 0; i < image.getWidth(); i++) {
             for (int j = 0; j < image.getHeight(); j++) {
-                image.setRGB(i, j, fractal.getColorAt(i * stepSizeW + bounds.get().x(), j * stepSizeH + bounds.get().y()).getRGB());
+                image.setRGB(i, j, fractal.getColorAt(i * stepSizeW + bounds.x(), j * stepSizeH + bounds.y()).getRGB());
             }
         }
 
